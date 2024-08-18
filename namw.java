60d4cd6b04be24d17547ccdcd297297fa58d7606
P-Jayashree06 <jayashree122005@gmail.com>
2024-08-18 20:01:17 +0530

    Add files via upload

---
+++ namw.java
@@ -0,0 +1,8 @@
+public class namw {
+    public static void main(String[] args) {
+        System.out.println("first line");
+        System.out.println("second line");
+        System.out.println("third line");
+        System.out.println("fourth line");
+    }
+}
